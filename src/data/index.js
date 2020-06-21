1d0f9c1adcf6bc76b403b83b9041512c939fbaa6
emmadal <emmanueldalougou@gmail.com>
2020-06-21 14:07:50 +0000

    add greeting hours

--- src/data/index.js
+++ src/data/index.js
@@ -1,70 +1,81 @@
-import {faTwitter, faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
+import {
+  faTwitter,
+  faLinkedin,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons'
 
 const data = [
-    {
-        title: 'GraphQL server without database',
-        description: 'GraphQL Server build with appolo-server that allow us to add, delete, update and querie the data into the database',
-        technology: 'Javascript, NodeJS, GraphQL',
-        link: 'https://github.com/emmadal/graphql-server'
-    },
-    {
-        title: 'GraphQL server with MongoDB database',
-        description: 'A GraphQL API connected to a MongoDB database',
-        technology: 'Javascript, NodeJS, GraphQL',
-        link: 'https://github.com/emmadal/graphmongo'
-    },
-    {
-        title: 'Movie Master',
-        description: 'Provide a website for the movies',
-        technology: 'React, Javascript',
-        link: 'https://moviemaster.netlify.com/'
-    },
-    {
-        title: 'Music Master',
-        description: 'Provide a website for listen the resume of music',
-        technology: 'React, Javascript',
-        link: 'https://emmadal.github.io/musicmaster/'
-    },
-    {
-        title: 'Port Scan',
-        description: 'PortScan is a port scanner build in javascript',
-        technology: 'Javascript, NodeJS',
-        link: 'https://github.com/emmadal/portscan'
-    },
-    {
-        title: 'AIPDC (Ivorian Association of Civil Drone Pilots)',
-        description: 'Provide a website for the UAV drivers in Ivory coast',
-        technology: 'Wordpress',
-        link: 'http://aipdc.net'
-    },
-    {
-        title: 'PROInvest',
-        description: 'PROInvest + is a program developed by LUMEN CORPORATION SAS which aims to connect local investors (employees, civil servants, traders) and local entrepreneurs (SMEs, Startups). This call for projects aims to offer entrepreneurs the possibility of bidding in order to obtain funding for the establishment or development of their projects.',
-        technology: 'HTML, CSS, Javascript',
-        link: 'http://pi.lumen-corporation.com/'
-    }
+  {
+    title: 'Deno REST API',
+    description: 'REST API with Deno, Oak and MongoDB write in Typescript',
+    technology: 'React, Typescript, MongoDB',
+    link: 'https://github.com/emmadal/api-activities',
+  },
+  {
+    title: 'Movie Master',
+    description: 'Provide a website for the movies',
+    technology: 'React, Javascript',
+    link: 'https://moviemaster.netlify.com/',
+  },
+  {
+    title: 'Music Master',
+    description: 'Provide a website for listen the resume of music',
+    technology: 'React, Javascript',
+    link: 'https://emmadal.github.io/musicmaster/',
+  },
+  {
+    title: 'GraphQL server without database',
+    description:
+      'GraphQL Server build with appolo-server that allow us to add, delete, update and querie the data into the database',
+    technology: 'Javascript, NodeJS, GraphQL',
+    link: 'https://github.com/emmadal/graphql-server',
+  },
+  {
+    title: 'GraphQL server with MongoDB database',
+    description: 'A GraphQL API connected to a MongoDB database',
+    technology: 'Javascript, NodeJS, GraphQL',
+    link: 'https://github.com/emmadal/graphmongo',
+  },
+  {
+    title: 'Port Scan',
+    description: 'PortScan is a port scanner build in javascript',
+    technology: 'Javascript, NodeJS',
+    link: 'https://github.com/emmadal/portscan',
+  },
+  {
+    title: 'AIPDC (Ivorian Association of Civil Drone Pilots)',
+    description: 'Provide a website for the UAV drivers in Ivory coast',
+    technology: 'Wordpress',
+    link: 'http://aipdc.net',
+  },
+  {
+    title: 'PROInvest',
+    description:
+      'PROInvest + is a program developed by LUMEN CORPORATION SAS which aims to connect local investors (employees, civil servants, traders) and local entrepreneurs (SMEs, Startups). This call for projects aims to offer entrepreneurs the possibility of bidding in order to obtain funding for the establishment or development of their projects.',
+    technology: 'HTML, CSS, Javascript',
+    link: 'http://pi.lumen-corporation.com/',
+  },
 ]
 
 export const socialNetwork = [
-    {
-        link:"https://twitter.com/emmanuel_dal",
-        icon: faTwitter,
-        color:"#1DA1F2",
-        title:"Twitter"
-    },
-    {
-        link:"https://www.linkedin.com/in/emmanuel-dalougou-5b1a77156/",
-        icon:faLinkedin,
-        color:"#2867B2",
-        title:"Linkedin"
-    },
-    {
-        link:"https://github.com/emmadal",
-        icon: faGithub,
-        color:"#000",
-        title:"Github"
-    },
-
+  {
+    link: 'https://twitter.com/emmanuel_dal',
+    icon: faTwitter,
+    color: '#1DA1F2',
+    title: 'Twitter',
+  },
+  {
+    link: 'https://www.linkedin.com/in/emmanuel-dalougou-5b1a77156/',
+    icon: faLinkedin,
+    color: '#2867B2',
+    title: 'Linkedin',
+  },
+  {
+    link: 'https://github.com/emmadal',
+    icon: faGithub,
+    color: '#000',
+    title: 'Github',
+  },
 ]
 
 export default data
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import styled from 'styled-components'
 import '../styles/layout.css'
 
 const Container = styled.div`
-  text-align:'justify';
+  text-align: 'justify';
   font-family: 'IBM Plex Sans';
   font-size: 1.3rem;
   line-height: 1.9rem;
   margin-top: 40px;
 `
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Container>
-      <h1>Hi people <span role="img">✋</span>,</h1>
-        <h2 style={{ lineHeight: 1.6 }}>My name is Emmanuel Dalougou, Front End developer <span role="img">💻</span> living in Ivory Coast
+function IndexPage() {
+  const [message, setMessage] = useState('')
+
+  const greetingTime = () => {
+    const currentHour = new Date().getUTCHours()
+    const afternoon = 13
+    const morning = 24
+    const evening = 18
+    if (
+      currentHour <= morning &&
+      currentHour <= afternoon &&
+      currentHour < evening
+    ) {
+      setMessage('Good Morning ⏰')
+    }
+    if (currentHour >= afternoon && currentHour <= evening) {
+      setMessage('Good afternoon 🍲')
+    }
+    if (currentHour >= evening) {
+      setMessage('Good evening 🛌')
+    }
+  }
+  useEffect(() => {
+    greetingTime()
+  }, [])
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Container>
+        <h1>
+          Hi people <span role="img">✋</span>, {message}
+        </h1>
+        <h2 style={{ lineHeight: 1.6 }}>
+          My name is Emmanuel Dalougou, Front End developer{' '}
+          <span role="img">💻</span> living in Ivory Coast
         </h2>
-        <p>I love thing about Javascript, GraphQL, React, NodeJS and Typescript.</p>
-        <p>My hobbies are the football, music and the food <span role="img">😋</span>.</p>
+        <p>
+          I love thing about Javascript, GraphQL, React, NodeJS, Typescript and
+          Deno.
+        </p>
+        <p>
+          My hobbies are the football, music and the food{' '}
+          <span role="img">😋</span>.
+        </p>
         <p>Feel free to send me a message on my social network.</p>
-    </Container>
-  </Layout>
-)
+      </Container>
+    </Layout>
+  )
+}
 
 export default IndexPage
